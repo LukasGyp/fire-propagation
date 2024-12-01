d3f0fffaa60f5395f82669c84b52ed2ffc159fe7
Oguro Luke <luke.oguro@student-cs.fr>
2024-12-01 13:25:25 +0100

    calculate interaction and probability maps

--- src/propagate.py
+++ src/propagate.py
@@ -1,9 +1,15 @@
 import numpy as np
+import scipy.signal as sg
 
 distances = np.array([[np.sqrt(i**2 + j**2) for j in range(-2, 3)]for i in range(-2, 3)])        
 
 def filter():
     return np.exp(-distances**2/2)
 
-print(proba_distribution())
+def calculate_interaction(intensity):
+    return sg.convolve(intensity, filter(), mode='same') 
 
+PROBABILITY_COEFFICIENT = 0.5
+def _convert_interaction_to_probability(interaction):
+    return 1 - np.exp(-interaction*PROBABILITY_COEFFICIENT)
+convert_interaction_to_probability = np.frompyfunc(_convert_interaction_to_probability, 1, 1)
