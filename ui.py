7e41d120a3855d2307df7b31af573bbb2b664308
Oguro Luke <luke.oguro@student-cs.fr>
2024-12-01 16:19:52 +0100

    renewed gif

--- ui.py
+++ ui.py
@@ -7,8 +7,10 @@ def animate(state, interval=300, frames=100):
     def update(i):
         if i: plt.cla()
         state.evolve()
-        plt.imshow(state.intensity, cmap='afmhot', vmin=0, vmax=5)
-        plt.title(f'frame {i}/{frames}')
+        plt.imshow(state.intensity, cmap='afmhot', vmin=0, vmax=70)
+        plt.title(f'frame ({i}/{frames})')
+        plt.xticks([])
+        plt.yticks([])
         
     anim = FuncAnimation(fig, update, interval=interval, frames=frames)
     anim.save('docs/fire.gif', fps=60, writer='pillow')
